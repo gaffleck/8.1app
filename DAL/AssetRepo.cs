5e6996192deeda3a1e6184ae9c7758294d899475
unknown <GEOAFFL@WTB19558.rbauction.net>
2013-09-30 16:50:21 -0700

    #task initial commit

---
+++ DAL/AssetRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using DataAccess;
+using SQLite;
+
+namespace DAL
+{
+    public sealed class AssetRepo
+    {
+
+        private SQLiteAsyncConnection db;
+
+
+        public AssetRepo()
+        {
+            db = new SQLiteAsyncConnection(Path.Combine(Windows.Storage.ApplicationData.Current.TemporaryFolder.Path, "db.sqlite"));
+            seed();
+        }
+
+        private void seed()
+        {
+            var res = db.CreateTableAsync<Asset>().Result;
+            db.InsertAsync(new Asset
+            {
+                AssetId = 1,
+                Description = "test"
+            });
+        }
+
+        public object GetAsset(int id)
+        {
+            var ass =  db.FindAsync<Asset>(x => x.AssetId == id).Result;
+            return ass.Serialize(typeof(Asset)); 
+        }
+
+        public int Save(string asset)
+        {
+            var asset2 = asset.Deserialize(typeof (Asset));
+            return db.InsertAsync(asset2).Result;
+        }
+
+
+    }
+}
---
+++ DAL/Extensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace DAL
+{
+    public static class Extensions
+    {
+
+
+        public static object Deserialize(this string json, Type type)
+        {
+            var _Bytes = Encoding.Unicode.GetBytes(json);
+            using (MemoryStream _Stream = new MemoryStream(_Bytes))
+            {
+                var _Serializer = new DataContractJsonSerializer(type);
+                return _Serializer.ReadObject(_Stream);
+            }
+        }
+
+        public static IDictionary<string,object> Serialize(this object instance, Type type)
+        {
+            var ret = new Dictionary<string, object>();
+            foreach (var prop in type.GetRuntimeProperties())
+            {
+                ret.Add(prop.Name, prop.GetValue(instance));
+            }
+            return ret; 
+        }
+
+    }
+}
